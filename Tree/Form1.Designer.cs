c76b79ae015102428061ae5fbfa024dd098d3228
tuankiet64 <trantuankiet071097@gmail.com>
2021-03-22 22:02:24 +0700

    first commit

---
+++ Tree/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.treeView2);
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 104);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(787, 334);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // treeView2
+            // 
+            this.treeView2.Location = new System.Drawing.Point(147, 22);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(629, 277);
+            this.treeView2.TabIndex = 1;
+            this.treeView2.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView2_AfterSelect);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(0, 22);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(141, 277);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "This PC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Tree/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tree
+{
+    public partial class Form1 : Form
+    {
+        public string driver = null;
+        public string current = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] drives = Directory.GetLogicalDrives();
+         
+
+            TreeNode[] array = new TreeNode[2];
+            int i = 0;
+            foreach (string drive in drives)
+            {
+                string disc = drive.Remove(drive.Length - 1);
+                array[i]= new TreeNode(disc);
+                i++;
+            }
+            TreeNode Disc = new TreeNode("Ổ Đĩa: ", array);
+            treeView1.Nodes.Add(Disc);
+        }
+
+        private void treeView2_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string text = e.Node.Text;
+            treeView2.Nodes.Clear();
+            string path = driver + text + "\\";
+            if (current != null)
+            {
+                path = current + text+"\\";
+            }
+            if (Directory.Exists(path))
+            {
+                var directories = Directory.GetDirectories(path);
+                foreach (string directorie in directories)
+                {
+                    var dir = new DirectoryInfo(directorie);
+                    var dirName = dir.Name;
+                    treeView2.Nodes.Add(new TreeNode(dirName));
+                }
+                current = path;
+                textBox1.Text = path;
+            }
+            
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            string text = e.Node.Text;
+            if (text != "Ổ Đĩa: ")
+            {
+                treeView2.Nodes.Clear();
+                driver = null;
+                current = null;
+                driver = text + "\\";
+                var directories = Directory.GetDirectories(text + "\\");
+                foreach (string directorie in directories)
+                {
+                    treeView2.Nodes.Add(new TreeNode(directorie.Substring(3)));
+                }
+                textBox1.Text = (text + "\\");
+            }
+        }
+    }
+}
