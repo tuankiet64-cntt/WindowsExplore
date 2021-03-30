bf5ae2da5404122e4b329b980be64150dfbb9c9d
tuankiet64 <trantuankiet071097@gmail.com>
2021-03-30 23:28:25 +0700

    Update

--- Tree/Form1.Designer.cs
+++ Tree/Form1.Designer.cs
@@ -30,29 +30,21 @@ namespace Tree
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.treeView2 = new System.Windows.Forms.TreeView();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.treeView2);
+            this.groupBox1.Controls.Add(this.listView1);
             this.groupBox1.Controls.Add(this.treeView1);
             this.groupBox1.Location = new System.Drawing.Point(12, 104);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(787, 334);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            // 
-            // treeView2
-            // 
-            this.treeView2.Location = new System.Drawing.Point(147, 22);
-            this.treeView2.Name = "treeView2";
-            this.treeView2.Size = new System.Drawing.Size(629, 277);
-            this.treeView2.TabIndex = 1;
-            this.treeView2.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView2_AfterSelect);
             // 
             // treeView1
             // 
@@ -68,6 +60,15 @@ namespace Tree
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(776, 23);
             this.textBox1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(147, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(629, 277);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // Form1
             // 
@@ -88,9 +89,9 @@ namespace Tree
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TreeView treeView2;
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- Tree/Form1.cs
+++ Tree/Form1.cs
@@ -24,7 +24,6 @@ namespace Tree
         private void Form1_Load(object sender, EventArgs e)
         {
             string[] drives = Directory.GetLogicalDrives();
-         
 
             TreeNode[] array = new TreeNode[2];
             int i = 0;
@@ -34,50 +33,85 @@ namespace Tree
                 array[i]= new TreeNode(disc);
                 i++;
             }
-            TreeNode Disc = new TreeNode("Ổ Đĩa: ", array);
+            TreeNode Disc = new TreeNode("My Computer", array);
             treeView1.Nodes.Add(Disc);
         }
 
-        private void treeView2_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            string text = e.Node.Text;
-            treeView2.Nodes.Clear();
-            string path = driver + text + "\\";
-            if (current != null)
-            {
-                path = current + text+"\\";
-            }
-            if (Directory.Exists(path))
-            {
-                var directories = Directory.GetDirectories(path);
-                foreach (string directorie in directories)
-                {
-                    var dir = new DirectoryInfo(directorie);
-                    var dirName = dir.Name;
-                    treeView2.Nodes.Add(new TreeNode(dirName));
-                }
-                current = path;
-                textBox1.Text = path;
-            }
+        //private void treeView2_AfterSelect(object sender, TreeViewEventArgs e)
+        //{
+        //    string text = e.Node.Text;
+        //    treeView2.Nodes.Clear();
+        //    string path = driver + text + "\\";
+        //    if (current != null)
+        //    {
+        //        path = current + text+"\\";
+        //    }
+        //    if (Directory.Exists(path))
+        //    {
+        //        var directories = Directory.GetDirectories(path);
+        //        foreach (string directorie in directories)
+        //        {
+        //            var dir = new DirectoryInfo(directorie);
+        //            var dirName = dir.Name;
+        //            treeView2.Nodes.Add(new TreeNode(dirName));
+        //        }
+        //        current = path;
+        //        textBox1.Text = path;
+        //    }
             
-        }
+        //}
 
         private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            string text = e.Node.Text;
-            if (text != "Ổ Đĩa: ")
+           
+            string fullpath = e.Node.Text;
+            if(fullpath== "My Computer") return;
+            TreeNode parent = e.Node.Parent;
+            string[] arr = new string[4];
+            ListViewItem itm;
+            while (parent != null )
             {
-                treeView2.Nodes.Clear();
+                if (parent.Text == "My Computer")
+                {
+                    fullpath =  fullpath;
+                    break;
+                }
+                else
+                    fullpath = parent.Text + "\\" + fullpath;
+
+                parent = parent.Parent;
+            }
+         
                 driver = null;
                 current = null;
-                driver = text + "\\";
-                var directories = Directory.GetDirectories(text + "\\");
-                foreach (string directorie in directories)
+                driver = fullpath + "\\";
+                var directories = Directory.GetDirectories(fullpath + "\\");
+                foreach (string var in directories)
                 {
-                    treeView2.Nodes.Add(new TreeNode(directorie.Substring(3)));
+                    string[] fs = var.Split('\\');
+                    // Thêm node vào treeview
+                    TreeNode node = new TreeNode(fs[fs.Length - 1]);
+                    node.Text = fs[fs.Length - 1];
+                    node.ImageIndex = 1;
+                    node.SelectedImageIndex = 2;
+                    e.Node.Nodes.Add(node);
                 }
-                textBox1.Text = (text + "\\");
-            }
+
+                listView1.Items.Clear();
+            var folder = Directory.GetDirectories(fullpath + "\\");
+            foreach (string var in folder)
+                 {
+                Debug.WriteLine(var);
+                // thêm vào view
+                ListViewItem item = new ListViewItem(Path.GetFileName(var), 0);
+                item.SubItems.Add(File.GetCreationTime(var).ToLongDateString());
+                string[] type = var.Split('.');
+                item.SubItems.Add(type[type.Length - 1] + " file type");
+                listView1.Items.Add(item);
+                }
+
+            textBox1.Text = (fullpath + "\\");
+            
         }
     }
 }
